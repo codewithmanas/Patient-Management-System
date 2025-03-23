0e34de77498f331624413f8f771b6f34b7f4321a
Manas Ranjan Adha <manas865873@gmail.com>
2025-03-23 18:20:34 +0530

    implement delete patient endpoint

--- patient-service/src/main/java/com/codewithmanas/patientservice/controllers/PatientController.java
+++ patient-service/src/main/java/com/codewithmanas/patientservice/controllers/PatientController.java
@@ -43,4 +43,12 @@ public class PatientController {
 
         return ResponseEntity.ok().body(patient);
     }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePatient(@PathVariable UUID id) {
+        patientService.deletePatient(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
